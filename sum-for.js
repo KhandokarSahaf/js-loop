34941b0fe7b4a23247a263a459fd91cb7bf3a2b0
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-29 20:30:50 +0600

    sum-for loop

---
+++ sum-for.js
@@ -0,0 +1,8 @@
+let sum = 0;
+
+for (let num = 1; num <= 10; num++) {
+  console.log(num);
+  sum = sum + num;
+}
+
+console.log(`sum of number 1 to 20 is: ${sum}`);
--- sum-while.js
+++ sum-while.js
@@ -4,6 +4,6 @@ let sum = 0;
 while (number <= 10) {
   console.log(number);
   sum = sum + number;
-  console.log(`sum : ${sum}`);
+  console.log(`sum: ${sum}`);
   number++;
 }
