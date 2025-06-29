81db727017289c9ddeac3fd64070992a3daa7a0c
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-29 17:58:54 +0600

    for Of loop

--- for-of.js
+++ for-of.js
@@ -1,7 +1,16 @@
+// 1st
 const num = [3, 4, 5, 6, 7, 8, 9];
 
 console.log(num);
 
 for (const roll of num) {
   console.log(roll);
+}
+
+// 2nd
+const fruits = ["apple", "guava", "banana", "watermelon"];
+
+for (const fruit of fruits) {
+  console.log("I want to eat");
+  console.log(fruit);
 }
