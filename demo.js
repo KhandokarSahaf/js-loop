fa837100a810ebd436c054cb79288900303fa67d
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-30 09:41:28 +0600

    console log final array

--- demo.js
+++ demo.js
@@ -1,5 +1,10 @@
-const fruit = ["apple", "pineapple", "guava", "jackfruit", "banana"];
+const fruits = ["apple", "pineapple", "guava", "jackfruit", "banana"];
 
-console.log(fruit);
+console.log(fruits);
+
 // console log the 3rd index element
-console.log(fruit[3]);
+console.log(fruits[3]);
+
+// change the value of the 2nd index element to Jambura
+fruits[2] = "jambura";
+console.log(fruits);
