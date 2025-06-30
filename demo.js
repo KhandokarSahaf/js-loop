c0bc8596f7637c86becc1270a69701031c509bd1
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-30 10:38:56 +0600

    Use the concat method to combine the two arrays into a new array. Print both the original arrays and the combined array using console.log().

--- demo.js
+++ demo.js
@@ -47,3 +47,17 @@ if (Array.isArray(num)) {
 } else {
   console.log(`this is not a array`);
 }
+
+// 5.***  Combining Arrays ***
+let aClass = ["s", "d", "f", "g", "h", "j", "k"];
+let bClass = ["z", "x", "c", "v", "b", "n", "m"];
+
+const abClass = aClass.concat(bClass);
+
+console.log(abClass);
+
+// Print both the original arrays and the combined array using console.log().
+
+console.log(`This is aClass`, aClass);
+console.log(`This is bClass`, bClass);
+console.log(`This is abClass`, abClass);
