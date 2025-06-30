f09021aef4ed5ace0171f3c0e673fd5db61752f2
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-30 09:57:05 +0600

    display the final array as output

--- demo.js
+++ demo.js
@@ -21,3 +21,8 @@ console.log(touristPlace);
 // Add two more to your array
 touristPlace.push("Chattrogram", "Sylhet");
 console.log(touristPlace);
+
+// Remove the last tourist destination you have added
+touristPlace.pop();
+// display the final array as output
+console.log(touristPlace);
