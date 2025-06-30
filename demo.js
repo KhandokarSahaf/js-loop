0bc62654470dcc4d6c899fcebad26caf45893c86
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-30 10:29:35 +0600

    Now use isArray to check if each variable is an array &&&&& Print a message to the console indicating whether each variable is an array or not

--- demo.js
+++ demo.js
@@ -35,3 +35,15 @@ if (books.includes("javascript")) {
 } else {
   console.log("No javascript here..");
 }
+
+// 4.***  Checking if it's an Array ***
+let num = [2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(Array.isArray(num));
+
+// Print a message to the console indicating whether each variable is an array or not.
+if (Array.isArray(num)) {
+  console.log(`this is a array`);
+} else {
+  console.log(`this is not a array`);
+}
