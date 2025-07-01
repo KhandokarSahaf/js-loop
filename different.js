02bec829bc408bc18a7909f78cfabf5d5649b80c
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-01 12:00:44 +0600

    odd number in different js

---
+++ different.js
@@ -0,0 +1,18 @@
+// odd numbers \\
+for (let i = 0; i < 20; i++) {
+  if (i % 2 === 1) {
+    console.log(i);
+  }
+}
+
+// *** 2 nd Type *** \\
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+// *** 3rd type *** \\
+for (let i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
