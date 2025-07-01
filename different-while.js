452ef01d4d5d4078153c8862f59f3612de35643a
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-01 12:35:11 +0600

    different-while js for odd number printing

---
+++ different-while.js
@@ -0,0 +1,26 @@
+// odd number \\
+let i = 1;
+
+while (i <= 20) {
+  if (i % 2 === 1) {
+    console.log(i);
+  }
+  i++;
+}
+
+// *** 2nd option *** \\
+let p = 1;
+
+while (p <= 20) {
+  if (p % 2 === 0) {
+    console.log(p);
+  }
+  p++;
+}
+
+// *** 3rd option *** \\
+let x = 1;
+
+while (x <= 20) {
+  console.log(x);
+}
--- different.js
+++ different.js
@@ -1,5 +1,5 @@
 // odd numbers \\
-for (let i = 0; i < 20; i++) {
+for (let i = 1; i < 20; i++) {
   if (i % 2 === 1) {
     console.log(i);
   }
